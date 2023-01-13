184c2966e440368868b06e44a9cff63a72d1d94b
eclectic-coding <chuck@eclecticsaddlebag.com>
2023-01-13 15:47:00 -0500

    Add logout action

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -15,10 +15,20 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
 
   test "error is rendered for login with incorrect credentials" do
     post login_path, params: {
-    user: {
-      email: "wrong@example.com",
-      password: "password"
-    } }
-  assert_select ".notification", I18n.t("sessions.create.incorrect_details")
+      user: {
+        email: "wrong@example.com",
+        password: "password"
+      } }
+    assert_select ".notification", I18n.t("sessions.create.incorrect_details")
+  end
+
+  test "logging out redirects to the root url and deletes the session" do
+    log_in(@user)
+
+    assert_difference("@user.app_sessions.count", -1) { log_out }
+    assert_redirected_to root_path
+
+    follow_redirect!
+    assert_select '.notification', I18n.t('sessions.destroy.success')
   end
 end
